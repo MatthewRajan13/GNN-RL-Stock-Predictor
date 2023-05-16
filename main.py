afc9f5661d6b770e4f3fcf81acefaabb6c3594ee
MatthewRajan13 <89418442+MatthewRajan13@users.noreply.github.com>
2023-05-15 22:23:03 -0400

    Added SMA, RSI, and MACD as node features

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import ta
 import torch
 import warnings
 import requests
 import bs4 as bs
+import numpy as np
 import pandas as pd
 import yfinance as yf
 from torch_geometric.data import Data
@@ -21,25 +23,38 @@ START_DATE = '2022-05-01'
 def main():
     tickers = ['BAL', 'BNO', 'CANE', 'CORN', 'COW', 'CPER', 'IAU', 'JO', 'SLV',
                'SOYB', 'UGA', 'UNG', 'USO', 'WEAT']
-    filled_tickers = fill_sp(tickers)
+    filled_tickers, price= fill_sp(tickers)
 
     # Graph attributes
-    edge_index, edge_weights, correlation_matrix, node_features, node_labels = get_graph_attributes(filled_tickers, tickers)
+    edge_index, edge_weights, correlation_matrix, node_features, node_labels = get_graph_attributes(filled_tickers, price, tickers)
 
     data = Data(x=node_features, y=node_labels, edge_index=edge_index, edge_attr=edge_weights, num_nodes=len(tickers))
 
     plot_graph(data, tickers)
 
-    input_dim = 14
-    hidden_dim = 32
-    latent_dim = 14
+    # Define the dimensions for the autoencoder
+    input_dim = data.x.size(2)
+    hidden_dim = 64
+    encoding_dim = 16
 
-    model = GNNAutoEncoder(input_dim, hidden_dim, latent_dim)
+    # Create the autoencoder model
+    model = GNNAutoEncoder(input_dim, hidden_dim, encoding_dim)
+
+    # move to device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = model.to(device)
+    data = data.to(device)
+
+    # Set the model to training mode
+    model.train()
+
+    # Initialize an optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-    encoded_data = train_gnn(model, data, optimizer, 100)
+    # Train the model
+    num_epochs = 100
 
-    print(encoded_data)
+    encoded_data = train_gnn(model, data, optimizer, num_epochs)
 
 
 def get_sp_list():
@@ -60,11 +75,23 @@ def get_sp_list():
 
 
 def fill_sp(tickers):
-    data = yf.download(tickers, start=START_DATE)['Adj Close']
+    data = pd.DataFrame()
+    price = pd.DataFrame()
+    for ticker in tickers:
+        ticker_data = yf.download(ticker, start=START_DATE).fillna(0)
+        price[ticker] = ticker_data['Adj Close']
+        ticker_data['SMA'] = ta.trend.sma_indicator(ticker_data['Adj Close'], window=20).fillna(0)
+        ticker_data['RSI'] = ta.momentum.rsi(ticker_data['Adj Close']).fillna(0)
+        ticker_data['MACD'] = ta.trend.macd(ticker_data['Adj Close']).fillna(0)
 
-    data = data.fillna(0)
+        scaler = MinMaxScaler()
+        ticker_data = pd.DataFrame(scaler.fit_transform(ticker_data),
+                                 columns=ticker_data.columns, index=ticker_data.index)
+        data[ticker] = pd.Series(ticker_data[['Adj Close', 'SMA', 'RSI', 'MACD']].values.tolist(), index=ticker_data.index)
 
-    return data
+    data.index.name = 'Date'
+
+    return data, price
 
 
 def get_adj_matrix(tickers):
@@ -74,8 +101,8 @@ def get_adj_matrix(tickers):
     return adj_matrix
 
 
-def get_graph_attributes(filled_tickers, tickers):
-    adj_matrix = get_adj_matrix(filled_tickers)
+def get_graph_attributes(filled_tickers, price, tickers):
+    adj_matrix = get_adj_matrix(price)
     adj_matrix = adj_matrix.fillna(0)
     adj_array = adj_matrix.to_numpy()
     adj_tensor = torch.from_numpy(adj_array).float()
@@ -86,11 +113,7 @@ def get_graph_attributes(filled_tickers, tickers):
     edge_index = edge_index[:, mask]
     edge_weights = edge_weights[mask]
 
-    # Load node features and labels into tensors
-    scaler = MinMaxScaler()
-    scaled_df = pd.DataFrame(scaler.fit_transform(filled_tickers),
-                                         columns=filled_tickers.columns, index=filled_tickers.index)
-    node_features = torch.tensor(scaled_df.values, dtype=torch.float)
+    node_features = torch.tensor(np.array(filled_tickers.values.tolist()), dtype=torch.float)
     node_labels = torch.tensor(range(len(tickers)))
 
     return edge_index, edge_weights, adj_matrix, node_features, node_labels
@@ -134,23 +157,22 @@ def plot_graph(data, tickers):
 
 
 def train_gnn(model, data, optimizer, num_epochs):
-    x = data.x.float()
-    edge_index = data.edge_index.long()
-
     for epoch in range(num_epochs):
-        model.train()
         optimizer.zero_grad()
-        x_hat = model(x, edge_index)
-        loss = F.mse_loss(x_hat, x)
+        output = model(data.x, data.edge_index)
+        loss = F.mse_loss(output, data.x)
         loss.backward()
         optimizer.step()
 
         if (epoch + 1) % 10 == 0:
             print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item()}")
 
+    # Set the model to evaluation mode
     model.eval()
+
+    # Encode the input data
     with torch.no_grad():
-        encoded_data = model.encode(x, edge_index)
+        encoded_data = model(data.x, data.edge_index)
 
     return encoded_data
 
